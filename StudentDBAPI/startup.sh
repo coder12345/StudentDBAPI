a0399fea6a12153b5a3b8a069d968ab6e8f9edcc
hellopi2 <jameslego2014@gmail.com>
2025-12-02 04:29:18 -0600

    Delete StudentDBAPI directory

--- StudentDBAPI/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install dependencies (if not already installed)
-pip install -r requirements.txt
-
-# Start FastAPI app
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:$PORT
